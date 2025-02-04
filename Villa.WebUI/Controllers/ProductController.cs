99cdc62473d823451c7be2ec15a6cad6c6086a7d
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2025-02-04 06:17:50 +0300

    Add project files.

---
+++ Villa.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.MessageDtos;
+using Villa.Dto.Dtos.ProductDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _productService.TGetListAsync();
+            var messageList = _mapper.Map<List<ResultProductDto>>(values);
+            return View(messageList);
+        }
+
+        public async Task<IActionResult> DeleteProduct(ObjectId id)
+        {
+            await _productService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var newProduct = _mapper.Map<Product>(createProductDto);
+            await _productService.TCreateAsync(newProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Villa.WebUI/Controllers/QuestController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.MessageDtos;
+using Villa.Dto.Dtos.QuestDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.WebUI.Controllers
+{
+    public class QuestController : Controller
+    {
+        private readonly IQuestService _questService;
+        private readonly IMapper _mapper;
+
+        public QuestController(IQuestService questService, IMapper mapper)
+        {
+            _questService = questService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _questService.TGetListAsync();
+            var questList = _mapper.Map<List<ResultQuestDto>>(values);
+            return View(questList);
+        }
+
+        public async Task<IActionResult> DeleteQuest(ObjectId id)
+        {
+            await _questService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateQuest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateQuest(CreateQuestDto createQuestDto)
+        {
+            var newQuest = _mapper.Map<Quest>(createQuestDto);
+            await _questService.TCreateAsync(newQuest);
+            return RedirectToAction("Index");
+        }
+    }
+}
